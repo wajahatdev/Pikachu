a6dc7ec15ad2c7082e8daa0a8baa83b5cc0d3fc5
Wajahat Shahid <wajahatdev@gmail.com>
2020-10-10 22:19:41 +0500

    Added the Neceesary Script

---
+++ Havana.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void IA()
+{
+	for(int i=0;i<10;i++)
+	cout<<"Peeno Mere Naal Weya Kr Ley"<<endl;
+	cout<<endl;
+}
+
+int main ()
+{
+	IA();
+	for (int i=0;i<5;i++)
+		cout<<"I love Pakistan."<<endl;
+	cout<<endl;
+	IA();
+	//Wajahat Shahid
+}
