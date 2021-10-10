735619ff06ceefb2d0b9654a0dedfba06af64197
Hammad Shahid <71876842+dr-wajahat@users.noreply.github.com>
2021-10-10 23:08:51 +0500

    Update Havana.cpp

--- Havana.cpp
+++ Havana.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void IA()
 {
-	for(int i=0;i<10;i++)
+	for(int i=1;i<=10;i++)
 	cout<<"Peeno Mere Naal Weya Kr Ley"<<endl;
 	cout<<endl;
 }
 
 void Lub()
 {
-	for (int i=0;i<5;i++)
+	for (int i=1;i<=5;i++)
 		cout<<"I love Pakistan."<<endl;
 	cout<<endl;
 }
